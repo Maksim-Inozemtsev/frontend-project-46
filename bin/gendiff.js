5c0d2731acc026145bb677f5ca60eda16d052891
Maksim Inozemtsev <imn46@mail.ru>
2023-02-10 12:36:08 +0600

    fix .eslintrc.yml & gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,6 +12,8 @@ program
   .version('1.0.0')
   .option('-f, --format <type>', 'output format: stylish, plain', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2, options) => console.log(genDiff(filepath1, filepath2, options.format)));
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
+  });
 
 program.parse(process.argv);
